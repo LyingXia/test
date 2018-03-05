1ae8dc9106058d1179b4cd3332ed88947879f65e
13810765094@163.com <8023guawohuide>
2018-03-05 09:57:35 +0800

    Demo中实验了Date的用法，然后testcase1跑通了selenium自动化添加人工充值待审核记录，并且添加优惠券的操作

--- src/main/java/com/test/applications/katalon/testcase1.java
+++ src/main/java/com/test/applications/katalon/testcase1.java
@@ -3,13 +3,13 @@ package com.test.applications.katalon;
 /**
  * created by xiapf on 2018/2/6
  */
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,30 +24,35 @@ public class testcase1 {
     @Before
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver","D:\\work\\file\\这个文件夹 今天开始属于我\\新人工作常用软件\\chromedriver.exe");
-        driver = new ChromeDriver();
+        //下面是实现chromedriver窗口最大化的方式 ChromeOptions
+        ChromeOptions opiions=new ChromeOptions();
+        opiions.addArguments("--start-maximized");
+        driver = new ChromeDriver(opiions);
         baseUrl = "http://zt-mgr.aetest.bwae.org";
-        driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
     }
 
     @Test
     public void testUntitledTestCase() throws Exception {
         driver.get(baseUrl);
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/form/div[1]/div/div[1]/input")).clear();
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/form/div[1]/div/div[1]/input")).sendKeys("test");
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/form/div[1]/div/div[1]/input")).sendKeys("chen");
         driver.findElement(By.xpath("//input[@type='password']")).clear();
-        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(":\"123456789a");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123456");
         driver.findElement(By.cssSelector("span")).click();
         /*点击业务管理*/
         //*[@id="app"]/div/div[2]/div[2]/div[2]/div[2]/div[4]/iframe
-        WebDriverWait wait = new WebDriverWait(driver,100);
-        wait.until(new ExpectedCondition<WebElement>(){
+        //WebDriverWait wait = new WebDriverWait(driver, 100);
+        /*wait.until(new ExpectedCondition<WebElement>() {
             @Override
             public WebElement apply(WebDriver d) {
                 return d.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div[2]/ul/div[4]/li/div/span"));
-            }}).click();
-        //driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div[2]/ul/div[4]/li/div/span")).click();
+            }
+        }).click();*/
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div[2]/ul/div[3]/li/div/span")).click();
         /*点击人工充值*/
-        driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div/div[2]/ul/div[4]/li/ul/div[10]/li")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div/div[2]/ul/div[3]/li/ul/div[10]/li")).click();
         //driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div/button/span")).click();
         /*这里有一步切换到iframe步骤*/
         WebElement iframe_tab = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[2]/div[2]/div[2]/div[2]/iframe"));
@@ -59,21 +64,40 @@ public class testcase1 {
         driver.findElement(By.cssSelector("div.el-form-item.is-required > div.el-form-item__content > div.el-input.el-input--small > input.el-input__inner")).sendKeys("2016092000352139");
         driver.findElement(By.xpath("(//input[@type='text'])[9]")).clear();
         driver.findElement(By.xpath("(//input[@type='text'])[9]")).sendKeys("100");
+
         //因为这个新的mgr的下拉框限制，所以，只能通过下面两行来进行下拉框的选择---加款类别
+        //python中用到的是ActionChains，java中用到的是Actions
+       /* WebElement addmenu1 = driver.findElement(By.xpath("(//input[@type='text'])[10]"));
+        new Actions(driver).click(addmenu1).perform();
+        WebElement addlist1 = driver.findElement(By.xpath("//div[3]/div/div/ul/li[4]"));
+        new Actions(driver).click(addlist1).perform();
+
+        WebElement addmenu2 = driver.findElement(By.xpath("(//input[@type='text'])[11]"));
+        new Actions(driver).click(addmenu2).perform();
+        WebElement addlist2 = driver.findElement(By.xpath("//div[4]/div/div/ul/li[1]"));
+        new Actions(driver).click(addlist2).perform();*/
         driver.findElement(By.xpath("(//input[@type='text'])[10]")).click();
-        driver.findElement(By.xpath("//div[3]/div/div/ul/li[4]")).click();
-//        wait.until(new ExpectedCondition<WebElement>(){
-//            @Override
-//            public WebElement apply(WebDriver d) {
-//                return d.findElement(By.xpath("//div[3]/div/div/ul/li[4]"));
-//            }}).click();
+        Thread.sleep(300);
+//        new Actions(driver).click(driver.findElement(By.xpath("(//input[@type='text'])[10]"))).perform();
+        driver.findElement(By.xpath("//div[3]/div/div[1]/ul/li[4]")).click();
+/*        wait.until(new ExpectedCondition<WebElement>() {
+            @Override
+            public WebElement apply(WebDriver d) {
+                return d.findElement(By.xpath("//div[3]/div/div/ul/li[4]"));
+            }
+        });*/
+        Thread.sleep(300);
         driver.findElement(By.xpath("(//input[@type='text'])[11]")).click();
+//        new Actions(driver).click(driver.findElement(By.xpath("(//input[@type='text'])[11]"))).perform();
+        Thread.sleep(300);
         driver.findElement(By.xpath("//div[4]/div/div/ul/li[1]")).click();
-//        wait.until(new ExpectedCondition<WebElement>(){
-//            @Override
-//            public WebElement apply(WebDriver d) {
-//                return d.findElement(By.xpath("//div[4]/div/div/ul/li[1]"));
-//            }}).click();
+/*        wait.until(new ExpectedCondition<WebElement>() {
+            @Override
+            public WebElement apply(WebDriver d) {
+                return d.findElement(By.xpath("//div[4]/div/div/ul/li[1]"));
+            }
+        });*/
+
 
 
        /* //普通的select可能遇到的下拉框难题，但是不适用于当前的这里，
@@ -88,13 +112,19 @@ public class testcase1 {
         driver.findElement(By.xpath("(//input[@type='text'])[12]")).sendKeys("测试");
         driver.findElement(By.cssSelector("div.dialog-footer > button.el-button.el-button--info")).click();
         driver.findElement(By.xpath("//div[3]/button[2]/span")).click();
-        // ERROR: Caught exception [ERROR: Unsupported command [selectFrame | relative=parent | ]]
-        driver.findElement(By.cssSelector("input.el-input__inner")).click();
-        driver.findElement(By.cssSelector("input.el-input__inner")).clear();
-        driver.findElement(By.cssSelector("input.el-input__inner")).sendKeys("优惠券");
-        driver.findElement(By.cssSelector("div.el-submenu__title")).click();
-        driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div/div[2]/ul/div/li/ul/div[2]/li")).click();
-        // ERROR: Caught exception [ERROR: Unsupported command [selectFrame | index=2 | ]]
+
+        //从iframe切换回来
+        driver.switchTo().parentFrame();
+        /*driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div[1]/span/div/input")).click();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div[1]/span/div/input")).clear();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div[1]/span/div/input")).sendKeys("优惠券");
+        driver.findElement(By.cssSelector("div.el-submenu__title")).click();*/
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div[2]/ul/div[11]/li/div/span")).click();
+        Thread.sleep(300);
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div[2]/ul/div[11]/li/ul/div[2]/li/span")).click();
+
+        WebElement iframe_tab2 = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/iframe"));
+        driver.switchTo().frame(iframe_tab2);
         driver.findElement(By.cssSelector("button.el-button.el-button--info.el-button--mini")).click();
         driver.findElement(By.cssSelector("div.el-form-item.is-required > div.el-form-item__content > div.el-input.el-input--small > input.el-input__inner")).click();
         driver.findElement(By.cssSelector("div.el-form-item.is-required > div.el-form-item__content > div.el-input.el-input--small > input.el-input__inner")).clear();
@@ -105,15 +135,23 @@ public class testcase1 {
         driver.findElement(By.xpath("(//input[@type='text'])[8]")).sendKeys("测试");
         driver.findElement(By.xpath("(//input[@type='text'])[9]")).clear();
         driver.findElement(By.xpath("(//input[@type='text'])[9]")).sendKeys("测试");
-        driver.findElement(By.cssSelector("td.available.today.current")).click();
+        driver.findElement(By.xpath("(//input[@type='text'])[10]")).click();
+        Thread.sleep(300);
+        WebElement now = driver.findElement(By.cssSelector("td.available.today.current"));
+        now.click();
         driver.findElement(By.xpath("(//input[@type='text'])[11]")).click();
-        driver.findElement(By.xpath("//div[4]/div/div/div[2]/table/tbody/tr[4]/td[3]")).click();
-        driver.findElement(By.cssSelector("div.el-dialog__body > form.el-form.el-form--label-right > div.el-row.el-row--flex > div.el-col.el-col-24 > div.el-form-item > div.el-form-item__content > div.el-select > div.el-input.el-input--small > input.el-input__inner")).click();
-        driver.findElement(By.cssSelector("li.el-select-dropdown__item.hover")).click();
-        driver.findElement(By.cssSelector("div.dialog-footer > button.el-button.el-button--primary > span")).click();
-        driver.findElement(By.xpath("(//button[@type='button'])[24]")).click();
+        Thread.sleep(300);
+        driver.findElement(By.xpath("//div[4]/div[1]/div/div[2]/table[1]/tbody/tr[6]/td[7]")).click();
+        //driver.findElement(By.cssSelector("body > div:nth-child(11) > div.el-picker-panel__body-wrapper > div > div.el-picker-panel__content > table.el-date-table > tbody > tr:nth-child(7) > td:nth-child(7)")).click();
+        //下面是选择产品类型的可以不写
+/*        driver.findElement(By.xpath("(//input[@type='text'])[12]")).click();
+        Thread.sleep(300);
+        driver.findElement(By.xpath("//div[6]/div/div[1]/ul/li[1]")).click();
+        driver.findElement(By.cssSelector("div.dialog-footer > button.el-button.el-button--primary > span")).click();*/
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div/div[3]/div/button[2]/span")).click();
+        String str = driver.findElement(By.xpath("/html/body/div[4]/div/div[2]/div[2]/p")).getText();
+        System.out.println(str);
     }
-
     @After
     public void tearDown() throws Exception {
         driver.quit();
--- src/main/java/com/test/sometest/Demo.java
+++ src/main/java/com/test/sometest/Demo.java
@@ -1,5 +1,8 @@
 package com.test.sometest;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by xiapf on 2017/2/17 0017.
  */
@@ -11,4 +14,11 @@ public class Demo{
     public Demo(){
         i++;
     }
+
+
+public static void main(String []args){
+    Date date = new Date();
+    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+    System.out.println(df.format(date));
+}
 }
